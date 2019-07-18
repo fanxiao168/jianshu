cb2cd303cb10ded9d5f6870aa0062b3ddc5759a0
fanxiao168 <fanxiao168@126.com>
2019-07-18 23:18:14 +0800

    项目目录搭建，Styled-Components与Reset.css的结合使用

---
+++ jianshu/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {GlobalStyled} from './style.js';
+
+function App() {
+  return (
+    <div>
+     hello world!
+     <GlobalStyled />
+    </div>
+  );
+}
+
+export default App;
