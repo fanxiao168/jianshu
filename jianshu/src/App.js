7c9509df7b7d8e69bd50a0ea116cf378cbf8bb7c
fanxiao168 <fanxiao168@126.com>
2019-07-23 17:47:58 +0800

    首页组件的拆分

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './common/header';
+import Home from './pages/home';
+import Detail from './pages/detail';
 import {GlobalStyled} from './style.js';
 import { IconFontStyled } from './statics/iconfont/iconfont';
 import store from './store';
@@ -12,8 +14,8 @@ function App() {
       <Provider store={store}>
 	      <Header/>
 	      <BrowserRouter>
-	      	 <Route path='/' exact  render={()=><div>home</div>}></Route>
-	      	 <Route path='/detail' exact render={()=><div>detail</div>}></Route>
+	      	 <Route path='/' exact  component={Home}></Route>
+	      	 <Route path='/detail' exact component={Detail}></Route>
 	      </BrowserRouter>
 	      <GlobalStyled/>
 	      <IconFontStyled/>
