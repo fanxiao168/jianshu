7060fcc3fe04fe26281dbf2a6260338f45bdfdba
fanxiao168 <fanxiao168@126.com>
2019-07-23 11:05:39 +0800

    热门搜索换页功能实现

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -24,21 +24,36 @@ class Header extends Component {
 
 	getListArea() {
 
-		const { focused, list } = this.props;
+		const { focused, mouseIn, list , totalPage, page , handleMouseEnter , handleMouseLeave , handleChangePage } = this.props;
+		const newList = list.toJS();
+		const pageList = [];
 
-		if (focused) {
+		if (newList.length > 0) {
+
+			for (let i = (page - 1) * 10 ; i < page * 10; i++) {
+				pageList.push(
+					<SearchInfoItem key={ newList[i] }>{ newList[i] }</SearchInfoItem>
+				)
+			}
+		}
+		
+		if (focused || mouseIn) {
 			return (
-				<SearchInfo>
+				<SearchInfo 
+					onMouseEnter={handleMouseEnter}
+					onMouseLeave={handleMouseLeave}
+				>
 					<SearchInfoTitle>
 					  热门搜索
-					<SearchInfoSwitch>换一批</SearchInfoSwitch>
+						<SearchInfoSwitch 
+						onClick={() => handleChangePage(page,totalPage)}
+						>
+						换一批
+						</SearchInfoSwitch>
+
 					</SearchInfoTitle>
 				 	<SearchInfoList>
-				 		{
-				 			list.map((item) => {
-				 			   return <SearchInfoItem key={ item }>{ item }</SearchInfoItem>
-				 			})
-				 		}
+				 		{ pageList }
 				 	</SearchInfoList>
 				</SearchInfo>
 			)
@@ -98,7 +113,10 @@ class Header extends Component {
 const mapStateToProps = (state) => {
 	return {
 		focused : state.getIn(['header','focused']),
-		list: state.getIn(['header','list'])
+		list: state.getIn(['header','list']),
+		totalPage: state.getIn(['header','totalPage']),
+		page: state.getIn(['header','page']),
+		mouseIn:state.getIn(['header','mouseIn'])
 	}
 }
 
@@ -111,7 +129,26 @@ const mapDispathToProps = (dispatch) => {
 
 		handleInputBlur() {
 			dispatch(actionCreators.searchBlur());
+		},
+
+		handleMouseEnter() {
+			dispatch(actionCreators.mouseEnter());
+		},
+
+		handleMouseLeave() {
+			dispatch(actionCreators.mouseLeave());
+		},
+
+		handleChangePage(page,totalPage) {
+			if (page < totalPage) {
+				dispatch(actionCreators.changePage(page + 1));
+			}else{
+				dispatch(actionCreators.changePage(1));
+			}
+			
 		}
+
+
 	}
 }
 
